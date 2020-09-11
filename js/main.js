0e46ae2c7bd66534216ae987f96fcad75dd14db2
eraynai <elli@cinehackers.com>
2020-09-11 08:25:28 -0400

    changes made to the code

--- js/main.js
+++ js/main.js
@@ -5,17 +5,28 @@ let extraCanvas;
 function setup(){
     createCanvas(windowWidth, windowHeight);
     extraCanvas = createGraphics(windowWidth, windowHeight);
-    extraCanvas.background(255, 0, 0);
+    extraCanvas.clear();
     background(0);
 }
 
 function draw(){
-    image(extraCanvas, 0, 0);
-    //background(0);
+    
+    background(0);
     x += random(-5, 5);
     y += random(-5, 5);
     fill(255, 200);
     stroke(255);
     rectMode(CENTER);
     rect(x, y, 20, 20);
+
+
+if(mouseIsPressed){
+    extraCanvas.fill(255, 150);
+    extraCanvas.noStroke();
+    extraCanvas.ellipse(mouseX, mouseY, 40, 40);
+}
+
+
+image(extraCanvas, 0, 0);
+
 }
